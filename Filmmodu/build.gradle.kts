5cf76250fdd1d1aad192f40386d98c5577b4eea3
jssb1971 <vurucularihsan1@gmail.com>
2025-10-26 18:12:25 +0300

    Create build.gradle.kts for Filmmodu plugin
    Add initial configuration for Filmmodu plugin

---
+++ Filmmodu/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    authors = listOf("jssb1971")
+    description = "Filmmodu.vip için eklenti"
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.filmmodu.vip/favicon.ico"
+}
